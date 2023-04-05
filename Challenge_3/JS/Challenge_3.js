305b22d1c77e952819adba3d53333db9e539daac
Antario-Beukes <126138138+Antario-Beukes@users.noreply.github.com>
2023-04-05 15:05:00 +0200

    Update Challenge_3.js

--- Challenge_3/JS/Challenge_3.js
+++ Challenge_3/JS/Challenge_3.js
@@ -10,10 +10,10 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-const owed = parseInt(`'R' + ${leoBalance} + ${sarahBalance}\n`)
-const leo = `${leoName} ${leoSurname} Owed R  ${leoBalance}\n`
-const sarah = `${sarahName}  ${sarahSurname} Owed R  ${sarahBalance}\n`
-const total = "\nTotal amount owed: "
-const result = `${leo}  ${sarah} ${divider} ${total} ${owed}\n ${divider}`  
-
+const owed = parseFloat( (leoBalance * -1) + (sarahBalance * -1)).toFixed(2)
+const leo = `${leoName} ${leoSurname} Owed R ${leoBalance * -1}\n`
+const sarah = `${sarahName}${sarahSurname} Owed R  ${sarahBalance * -1}\n`
+const total = "\nTotal amount owed: R "
+const result = `${leo}\n  ${sarah}\n ${divider}\n ${total} ${owed}\n\n ${divider}`  
+               
 console.log(result)
